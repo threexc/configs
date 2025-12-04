3e04d8a918f8a4d875d745a5700fffda798fa977
Trevor Gamblin <tvgamblin@gmail.com>
2025-12-03 20:46:13 -0500

    waybar: weather.sh: simplify output to text
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- .config/waybar/modules/weather.sh
+++ .config/waybar/modules/weather.sh
@@ -36,40 +36,40 @@ temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]]+)\.\./\1 to /g')
 # https://fontawesome.com/icons?s=solid&c=weather
 case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
 "clear" | "sunny")
-    condition=""
+    condition="clear"
     ;;
 "partly cloudy")
-    condition=""
+    condition="partly cloudy"
     ;;
 "cloudy")
-    condition=""
+    condition="cloudy"
     ;;
 "overcast")
-    condition=""
+    condition="overcast"
     ;;
 "mist" | "fog" | "freezing fog")
-    condition=""
+    condition="fog"
     ;;
 "patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain")
-    condition=""
+    condition="light rain"
     ;;
 "moderate rain at times" | "moderate rain" | "heavy rain at times" | "heavy rain" | "moderate or heavy rain shower" | "torrential rain shower" | "rain shower")
-    condition=""
+    condition="rain"
     ;;
 "patchy snow possible" | "patchy sleet possible" | "patchy freezing drizzle possible" | "freezing drizzle" | "heavy freezing drizzle" | "light freezing rain" | "moderate or heavy freezing rain" | "light sleet" | "ice pellets" | "light sleet showers" | "moderate or heavy sleet showers")
-    condition=""
+    condition="ice"
     ;;
 "blowing snow" | "moderate or heavy sleet" | "patchy light snow" | "light snow" | "light snow showers")
-    condition=""
+    condition="light snow"
     ;;
 "blizzard" | "patchy moderate snow" | "moderate snow" | "patchy heavy snow" | "heavy snow" | "moderate or heavy snow with thunder" | "moderate or heavy snow showers")
-    condition=""
+    condition="heavy snow"
     ;;
 "thundery outbreaks possible" | "patchy light rain with thunder" | "moderate or heavy rain with thunder" | "patchy light snow with thunder")
-    condition=""
+    condition="thunderstorms"
     ;;
 *)
-    condition=""
+    condition="unknown"
     echo -e "{\"text\":\""$condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
     ;;
 esac
